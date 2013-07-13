9b94fd920194acbe8d5a79a86195d7c163686080
WeightyOgre <WeightyOgre@gmail.com>
2013-07-13 21:22:52 +0100

    A font class that can be instantiated with different values and takes in a string and position

---
+++ FontClass/FontClass/FontClass/Game1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace FontClass
+{
+
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        TextDisplay aTest;
+
+        TextDisplay anotherTest;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            //test textdisplay class
+            aTest = new TextDisplay("String Test", new Vector2(0,100));
+            anotherTest = new TextDisplay("Another Strin Test", new Vector2(100,100));
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            aTest.Load(this.Content);
+            anotherTest.Load(this.Content);
+
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            SpriteBatch fontBatch = new SpriteBatch(GraphicsDevice);
+            aTest.DrawFont(fontBatch);
+
+            anotherTest.DrawFont(fontBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ FontClass/FontClass/FontClass/TextDisplay.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace FontClass
+{
+    class TextDisplay
+    {
+
+        //text output for testing purposes
+        SpriteFont Font1;
+
+        SpriteBatch spritebatch;
+
+        String aString;
+
+        Vector2 stringPosition;
+
+        public TextDisplay(String aString, Vector2 stringPosition)
+        {
+            this.aString = aString;
+            this.stringPosition = stringPosition;
+        }
+
+        public void Load(ContentManager content)
+        {
+            Font1 = content.Load<SpriteFont>("SpriteFont1");
+        }
+
+        public void DrawFont(SpriteBatch spritebatch)
+        {
+            this.spritebatch = spritebatch;
+            spritebatch.Begin();
+            spritebatch.DrawString(Font1,
+            aString,
+            stringPosition,
+            Color.Black);
+            spritebatch.End();
+        }
+
+    }
+}
